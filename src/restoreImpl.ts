52b3756560a03973dd33b2b34501ddfaaf5ad67f
Safoine El Khabich <34200873+safoinme@users.noreply.github.com>
2023-12-08 19:45:34 +0000

    refactor the code to save and restore from and to a pvc

--- src/restoreImpl.ts
+++ src/restoreImpl.ts
@@ -1,24 +1,19 @@
-import * as cache from "@actions/cache";
 import * as core from "@actions/core";
+import * as tar from "tar";
+import * as path from "path";
+import * as fs from "fs";
 
 import { Events, Inputs, Outputs, State } from "./constants";
-import {
-    IStateProvider,
-    NullStateProvider,
-    StateProvider
-} from "./stateProvider";
+import { IStateProvider, StateProvider } from "./stateProvider";
 import * as utils from "./utils/actionUtils";
 
-export async function restoreImpl(
-    stateProvider: IStateProvider
-): Promise<string | undefined> {
+export async function restoreImpl(stateProvider: IStateProvider): Promise<void> {
     try {
         if (!utils.isCacheFeatureAvailable()) {
             core.setOutput(Outputs.CacheHit, "false");
             return;
         }
 
-        // Validate inputs, this can cause task failure
         if (!utils.isValidEvent()) {
             utils.logWarning(
                 `Event Validation Error: The event type ${
@@ -31,70 +26,30 @@ export async function restoreImpl(
         const primaryKey = core.getInput(Inputs.Key, { required: true });
         stateProvider.setState(State.CachePrimaryKey, primaryKey);
 
-        const restoreKeys = utils.getInputAsArray(Inputs.RestoreKeys);
-        const cachePaths = utils.getInputAsArray(Inputs.Path, {
-            required: true
-        });
-        const s3BucketName = core.getInput(Inputs.AWSS3Bucket);
-        const s3config = utils.getInputS3ClientConfig();
+        const cachePaths = utils.getInputAsArray(Inputs.Path, { required: true });
+        const cacheDirectory = "/caching"; // Replace with your actual PVC mount path
+        const tarballPath = path.join(cacheDirectory, `${primaryKey}.tar`);
 
-        const enableCrossOsArchive = utils.getInputAsBool(
-            Inputs.EnableCrossOsArchive
-        );
-        const failOnCacheMiss = utils.getInputAsBool(Inputs.FailOnCacheMiss);
-        const lookupOnly = utils.getInputAsBool(Inputs.LookupOnly);
+        if (fs.existsSync(tarballPath)) {
+            await tar.extract({
+                gzip: true,
+                file: tarballPath,
+                cwd: process.cwd(),
+            });
 
-        const cacheKey = await cache.restoreCache(
-            cachePaths,
-            primaryKey,
-            restoreKeys,
-            { lookupOnly: lookupOnly },
-            enableCrossOsArchive,
-            s3config,
-            s3BucketName
-        );
-
-        if (!cacheKey) {
-            if (failOnCacheMiss) {
-                throw new Error(
-                    `Failed to restore cache entry. Exiting as fail-on-cache-miss is set. Input key: ${primaryKey}`
-                );
-            }
-            core.info(
-                `Cache not found for input keys: ${[
-                    primaryKey,
-                    ...restoreKeys
-                ].join(", ")}`
-            );
-
-            return;
-        }
-
-        // Store the matched cache key in states
-        stateProvider.setState(State.CacheMatchedKey, cacheKey);
-
-        const isExactKeyMatch = utils.isExactKeyMatch(
-            core.getInput(Inputs.Key, { required: true }),
-            cacheKey
-        );
-
-        core.setOutput(Outputs.CacheHit, isExactKeyMatch.toString());
-        if (lookupOnly) {
-            core.info(`Cache found and can be restored from key: ${cacheKey}`);
+            core.setOutput(Outputs.CacheHit, "true");
+            core.info(`Cache restored from key: ${primaryKey}`);
+            stateProvider.setState(State.CacheMatchedKey, primaryKey);
         } else {
-            core.info(`Cache restored from key: ${cacheKey}`);
+            core.setOutput(Outputs.CacheHit, "false");
+            core.info(`Cache not found for key: ${primaryKey}`);
         }
-
-        return cacheKey;
-    } catch (error: unknown) {
-        core.setFailed((error as Error).message);
+    } catch (error) {
+        core.setFailed(error.message);
     }
 }
 
-async function run(
-    stateProvider: IStateProvider,
-    earlyExit: boolean | undefined
-): Promise<void> {
+async function run(stateProvider: IStateProvider, earlyExit: boolean | undefined): Promise<void> {
     try {
         await restoreImpl(stateProvider);
     } catch (err) {
@@ -104,24 +59,15 @@ async function run(
         }
     }
 
-    // node will stay alive if any promises are not resolved,
-    // which is a possibility if HTTP requests are dangling
-    // due to retries or timeouts. We know that if we got here
-    // that all promises that we care about have successfully
-    // resolved, so simply exit with success.
     if (earlyExit) {
         process.exit(0);
     }
 }
 
-export async function restoreOnlyRun(
-    earlyExit?: boolean | undefined
-): Promise<void> {
-    await run(new NullStateProvider(), earlyExit);
+export async function restoreOnlyRun(earlyExit?: boolean | undefined): Promise<void> {
+    await run(new StateProvider(), earlyExit);
 }
 
-export async function restoreRun(
-    earlyExit?: boolean | undefined
-): Promise<void> {
+export async function restoreRun(earlyExit?: boolean | undefined): Promise<void> {
     await run(new StateProvider(), earlyExit);
 }
